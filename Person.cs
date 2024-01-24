62333510936f2e4980d815c8507b5df577e2ee92
laboratorio <laboratorio@UPOAULA10211>
2024-01-23 19:11:57 -0500

    Agregar archivos de proyecto.

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿public class Person : ICloneable
+{
+    public readonly string[] Names;
+    public readonly Address Address;
+
+    public Person()
+    {
+        Console.WriteLine("Person Initialized");   
+    }
+
+    public Person(string[] names, Address address)
+    {
+        Names = names;
+        Address = address;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+    }
+
+    public object Clone()
+    {
+        return new Person(Names, Address);
+    }
+
+    private new void MyMethod()
+    {
+        Console.WriteLine("This is private Method");
+    }
+}
